fc79b62562fe37626a53b45e1af0598df8035441
Jayanti gupta <jayantigupta.489@gmail.com>
2025-02-23 11:00:14 +0530

    patterns

---
+++ Pattern/pattern1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<stdio.h>
+int main(){
+ 
+    int i=1;
+    while (i<=5)
+    {
+        int j=5;
+        while (j>=1)
+        {
+         std::cout<<j;
+         j--;
+
+        }
+        
+     std::cout<<std::endl;
+     i++;
+    }
+    
+}
